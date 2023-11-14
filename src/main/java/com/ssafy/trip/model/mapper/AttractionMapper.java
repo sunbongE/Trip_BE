e4354e5b3c67666bf9c3c4f2af70f78f608cb371
306yyy <306yyy@naver.com>
2023-11-15 08:33:06 +0900

    refactor : 파라미터 매핑 어노테이션 추가

--- src/main/java/com/ssafy/trip/model/mapper/AttractionMapper.java
+++ src/main/java/com/ssafy/trip/model/mapper/AttractionMapper.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.ssafy.trip.model.AttractionInfoDto;
 import com.ssafy.trip.model.GugunDto;
 import com.ssafy.trip.model.SidoDto;
+
 @Mapper
 public interface AttractionMapper {
 
@@ -15,11 +17,16 @@ public interface AttractionMapper {
 
 //	List<AttractionInfoDto> searchByTitle(String title, int sidoCode);
 	String findUserId(Map<String, String> map);
+
 	String findUserPwd(Map<String, String> map);
+
 	// 시도 불러오기
 	List<SidoDto> sidoList();
+
 	// 구군 불러오기
 	List<GugunDto> gugunList(int sidoCode);
-	List<AttractionInfoDto> searchTourinfo(int sidoCode, int gugunCode, int contentTypeId);
+
+	List<AttractionInfoDto> searchTourinfo(@Param("sidoCode") int sidoCode, @Param("gugunCode") int gugunCode, @Param("contentTypeId") int contentTypeId);
+
 	List<AttractionInfoDto> searchByTitle(String title);
 }
