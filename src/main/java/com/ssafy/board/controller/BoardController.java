3861f3ceece77c6dbea19a6bd042e445c6313d57
박태호 <qkrxogh7@naver.com>
2023-11-16 14:36:49 +0900

    refactor: 페이지네이션 적용 (board)

--- src/main/java/com/ssafy/board/controller/BoardController.java
+++ src/main/java/com/ssafy/board/controller/BoardController.java
@@ -1,8 +1,10 @@
 package com.ssafy.board.controller;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,23 +24,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.board.model.BoardDto;
+import com.ssafy.board.model.BoardListDto;
 import com.ssafy.board.model.service.BoardService;
 import com.ssafy.util.BoardUtil;
+
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController // @Controller+Responsebody
 @RequestMapping("/board")
 @CrossOrigin("*")
+@Slf4j // log.사용할 때
 public class BoardController extends HttpServlet {
 
 	BoardService boardService;
 	BoardUtil boardUtil = BoardUtil.getInstance();
-	
-	
-	
-	
 
 	@Autowired
 	public BoardController(BoardService boardService) {
@@ -48,52 +54,49 @@ public class BoardController extends HttpServlet {
 	protected ResponseEntity<?> regist(@RequestBody BoardDto boardDto) {
 		try {
 			System.out.println(boardDto);
-			
 //			 비속어 필터
-				if (boardUtil.filterSlangs(boardDto.getContent())) {
-					System.out.println("비속어 감지");
-					return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
-				}
-				boardService.registerArticle(boardDto);
-//				List<BoardDto> list = boardService.searchListAll();
-				return new ResponseEntity<Void>(HttpStatus.CREATED);
-//			return new ResponseEntity<List<BoardDto>>(list, HttpStatus.OK);
+			if (boardUtil.filterSlangs(boardDto.getContent())) {
+				System.out.println("비속어 감지");
+				return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+			}
+			boardService.registerArticle(boardDto);
+			return new ResponseEntity<Void>(HttpStatus.CREATED);
 		} catch (Exception e) {
 			return exceptionHandling(e);
 		}
 	}
 
 	@GetMapping("/list")
-	protected ResponseEntity<?> list(HttpServletRequest request, HttpServletResponse response) {
-		List<BoardDto> list = boardService.searchListAll();
+	protected ResponseEntity<?> list(
+			@RequestParam @ApiParam(value = "게시글을 얻기위한 부가정보.", required = true) Map<String, String> map) {
 		try {
+			BoardListDto list = boardService.searchListAll(map);
 
-			if (list != null && !list.isEmpty()) {
-				return ResponseEntity.status(HttpStatus.OK).body(list);
-			} else {
-				return new ResponseEntity<List>(Collections.EMPTY_LIST, HttpStatus.OK);
-			}
+			HttpHeaders header = new HttpHeaders();
+			header.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+			return ResponseEntity.ok().headers(header).body(list);
 		} catch (Exception e) {
 			return exceptionHandling(e);
 		}
 	}
+
 	@GetMapping("/view/{articleNo}")
 	protected ResponseEntity<?> view(@PathVariable("articleNo") int articleNo) throws ServletException, IOException {
 		try {
 			BoardDto boardDto = boardService.viewArticle(articleNo);
-			if(boardDto!=null) {
+			if (boardDto != null) {
 				return new ResponseEntity<BoardDto>(boardDto, HttpStatus.OK);
-			}else {
+			} else {
 				return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 			}
 		} catch (Exception e) {
 			return exceptionHandling(e);
 		}
-	
+
 	}
 
 	@PutMapping("/modify")
-	protected  ResponseEntity<?> modify(@RequestBody BoardDto boardDto) {
+	protected ResponseEntity<?> modify(@RequestBody BoardDto boardDto) {
 		try {
 //			 비속어 필터
 			if (boardUtil.filterSlangs(boardDto.getContent())) {
@@ -101,28 +104,21 @@ public class BoardController extends HttpServlet {
 				return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 			}
 			boardService.modifyArticle(boardDto);
-//			System.out.println(boardDto);
-			List<BoardDto> list = boardService.searchListAll();
-			if (list != null && !list.isEmpty()) {
-				return ResponseEntity.status(HttpStatus.OK).body(list);
-			} else {
-				return new ResponseEntity<List>(Collections.EMPTY_LIST, HttpStatus.OK);
-			}
+			return new ResponseEntity<Void>(HttpStatus.OK);
 		} catch (Exception e) {
 			return exceptionHandling(e);
 		}
 	}
-	
+
 	@DeleteMapping("/delete/{articleNo}")
-    protected ResponseEntity<?> delete(@PathVariable int articleNo)
-            throws ServletException, IOException {
-        try {
-            boardService.deleteArticle(articleNo);
-            return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
-        }
-    }
+	protected ResponseEntity<?> delete(@PathVariable int articleNo) throws ServletException, IOException {
+		try {
+			boardService.deleteArticle(articleNo);
+			return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+		}
+	}
 
 	private ResponseEntity<String> exceptionHandling(Exception e) {
 		e.printStackTrace();
--- src/main/java/com/ssafy/board/model/service/BoardServiceImpl.java
+++ src/main/java/com/ssafy/board/model/service/BoardServiceImpl.java
@@ -1,11 +1,15 @@
 package com.ssafy.board.model.service;
 
-import java.util.List; 
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 import com.ssafy.board.model.BoardDto;
-import com.ssafy.board.model.mapper.BoardMapper; 
+import com.ssafy.board.model.BoardListDto;
+import com.ssafy.board.model.mapper.BoardMapper;
 @Service
 public class BoardServiceImpl implements BoardService {
 
@@ -22,9 +26,33 @@ public class BoardServiceImpl implements BoardService {
 	}
 
 	@Override
-	public List<BoardDto> searchListAll() {
-		// TODO Auto-generated method stub
-		return boardMapper.searchListAll();
+	public BoardListDto searchListAll(Map<String, String> map) throws SQLException {
+//		return boardMapper.searchListAll(map);
+		Map<String, Object> param = new HashMap<String, Object>();
+		param.put("word", map.get("word") == null ? "" : map.get("word"));
+		int currentPage = Integer.parseInt(map.get("pgno") == null ? "1" : map.get("pgno"));
+		int sizePerPage = Integer.parseInt(map.get("spp") == null ? "20" : map.get("spp"));
+		int start = currentPage * sizePerPage - sizePerPage;
+		param.put("start", start);
+		param.put("listsize", sizePerPage);
+
+		String key = map.get("key");
+		param.put("key", key == null ? "" : key);
+		if ("user_id".equals(key))
+			param.put("key", key == null ? "" : "b.user_id");
+		List<BoardDto> list = boardMapper.searchListAll(param);
+
+		if ("user_id".equals(key))
+			param.put("key", key == null ? "" : "user_id");
+		int totalArticleCount = boardMapper.getTotalArticleCount(param);
+		int totalPageCount = (totalArticleCount - 1) / sizePerPage + 1;
+
+		BoardListDto boardListDto = new BoardListDto();
+		boardListDto.setArticles(list);
+		boardListDto.setCurrentPage(currentPage);
+		boardListDto.setTotalPageCount(totalPageCount);
+
+		return boardListDto;
 	}
 
 	@Override
@@ -35,6 +63,7 @@ public class BoardServiceImpl implements BoardService {
 
 	@Override
 	public BoardDto viewArticle(int articleNo) {
+		boardMapper.updateHit(articleNo);
 		return boardMapper.viewArticle(articleNo);
 	}
 
